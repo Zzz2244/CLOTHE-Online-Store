a9fffcf8eaec3ef3d7748872da9abb4b3528ffcf
Zzz2244 <vinhthai20042007@gmail.com>
2026-05-15 22:18:17 +0700

    Update settings.py

--- Online Clothes Shop/onlineshop/settings.py
+++ Online Clothes Shop/onlineshop/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'onlineshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'neondb',
+        'NAME': 'shop_db',
         'USER': 'postgres',
         'PASSWORD': 'admin123',
         'HOST': 'localhost',
